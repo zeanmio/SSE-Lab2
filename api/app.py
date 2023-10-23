53dae3ab93efa8fd8e9c956c010e27427ebfeb1f
Leila Lin <xl6423@ic.ac.uk>
2023-10-23 16:55:28 +0100

    updated query

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def process_query(query):
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
-    else:
+    if query == "asteroids":
         return "Unknown"
 
 
