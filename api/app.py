e6de2a788cc73e03efc590036dff6aaeb5244a72
Leila Lin <xl6423@ic.ac.uk>
2023-10-30 17:17:11 +0000

    update

--- api/app.py
+++ api/app.py
@@ -3,6 +3,24 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_square_and_cube(n):
+    root = int(n**0.5)
+    if root * root == n:
+        cube_root = round(n ** (1 / 3))
+        if cube_root * cube_root * cube_root == n:
+            return True
+    return False
+
+
 def process_query(query):
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
@@ -30,6 +48,17 @@ def process_query(query):
             return str(sum(numbers))
         else:
             return "No numbers found in the query"
+    if "multiplied" in query:
+        numbers = [int(word) for word in query.split() if word.isdigit()]
+        return str(numbers[0] * numbers[1])
+    if "primes" in query:
+        numbers = [int(word) for word in query.split() if word.isdigit()]
+        primes = [num for num in numbers if is_prime(num)]
+        return ", ".join(map(str, primes))
+    if "square and a cube" in query:
+        numbers = [int(word) for word in query.split() if word.isdigit()]
+        result = [num for num in numbers if is_square_and_cube(num)]
+        return ", ".join(map(str, result))
 
 
 @app.route("/")
