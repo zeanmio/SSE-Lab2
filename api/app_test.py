6bab710ee054307e621740ec30433e1f49b8c32c
Leila Lin <xl6423@ic.ac.uk>
2023-10-30 15:52:23 +0000

    update

--- api/app_test.py
+++ api/app_test.py
@@ -1,16 +1,16 @@
 from app import process_query
+from api.app import app
 
 
 def test_knows_about_dinosaurs():
-    assert (
-        process_query("dinosaurs")
-        == "Dinosaurs ruled the Earth \
-        200 million years ago"
-    )
+    with app.app_context():
+        assert process_query("dinosaurs") == "Dinosaurs \
+            ruled the Earth 200 million years ago"
 
 
 def test_does_not_know_about_asteroids():
-    assert process_query("asteroids") == "Unknown"
+    with app.app_context():
+        assert process_query("asteroids") == "Unknown"
 
 
 def test_knows_team_name():
