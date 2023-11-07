671a59199dd3ebff6a58dbb18ad0bddc97dca4f1
Leila Lin <xl6423@ic.ac.uk>
2023-11-07 00:28:29 +0000

    search

--- api/app.py
+++ api/app.py
@@ -163,6 +163,8 @@ def hello_github_user():
 
     languages = list(language_counts.keys())
     counts = list(language_counts.values())
+    
+    print(repos_data)
 
     return render_template(
         "hello_github_user.html",
